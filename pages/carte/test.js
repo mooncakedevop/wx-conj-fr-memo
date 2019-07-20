7916e916e13b6970a563c9fb88116cd0d075cb05
HUANG Xiaodong <sheldonhuang@qq.com>
2019-07-21 00:50:05 +0200

    test查询变位云数据库

--- pages/carte/test.js
+++ pages/carte/test.js
@@ -1,7 +1,6 @@
 const app = getApp()
 const db = wx.cloud.database() //初始化数据库
-const verb = db.collection('conj_verb7016')
-const classic = db.collection('conj_classic149')
+const verb = db.collection('conj_all')
 
 Page({
   data: {
@@ -29,63 +28,129 @@ Page({
   search: function() {
     var search_word = this.data.input_word_conj;
     this.onQuery(search_word);
+    console.log(search_word)
   },
 
   onQuery: function(search_word) {
     var that = this
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
-    db.collection('conj_verb7016').where({
-      ow: search_word
-    }).get({
+
+    const _ = db.command
+    db.collection('conj_all').where(_.or([{
+        sw: search_word
+      },
+      {
+        condi_pre1: search_word
+      },
+      {
+        condi_pre2: search_word
+      },
+      {
+        condi_pre3: search_word
+      },
+      {
+        condi_pre4: search_word
+      },
+      {
+        condi_pre5: search_word
+      },
+      {
+        condi_pre6: search_word
+      },
+      {
+        imp_pre1: search_word
+      },
+      {
+        imp_pre2: search_word
+      },
+      {
+        indi_fu1: search_word
+      },
+      {
+        indi_fu2: search_word
+      },
+      {
+        indi_fu3: search_word
+      },
+      {
+        indi_fu4: search_word
+      },
+      {
+        indi_fu5: search_word
+      },
+      {
+        indi_fu6: search_word
+      },
+      {
+        indi_imp1: search_word
+      },
+      {
+        indi_imp2: search_word
+      },
+      {
+        indi_imp3: search_word
+      },
+      {
+        indi_imp4: search_word
+      },
+      {
+        indi_imp5: search_word
+      },
+      {
+        indi_imp6: search_word
+      },
+      {
+        indi_past1: search_word
+      },
+      {
+        indi_past2: search_word
+      },
+      {
+        indi_past3: search_word
+      },
+      {
+        indi_past4: search_word
+      },
+      {
+        indi_past5: search_word
+      },
+      {
+        indi_past6: search_word
+      },
+      {
+        indi_pre1: search_word
+      },
+      {
+        indi_pre2: search_word
+      },
+      {
+        indi_pre3: search_word
+      },
+      {
+        indi_pre4: search_word
+      },
+      {
+        indi_pre5: search_word
+      },
+      {
+        indi_pre6: search_word
+      },
+      {
+        inf_pre: search_word
+      }
+    ])).get({
       success: function(res) {
         console.log(res.data)
-        if (res.data.length === 0) {
 
-          db.collection('conj_verb7016').where({
-            sw: search_word
-          }).get({
-            success: function(res) {
-              console.log(res.data)
-              wx.setStorageSync('cw', res.data[0].cw);
-              wx.setStorageSync('ow', res.data[0].ow);
-              wx.setStorageSync('sw', res.data[0].sw);
-              wx.setStorageSync('tg', res.data[0].tg);
-              wx.setStorageSync('wn', res.data[0].wn);
-              wx.setStorageSync('wr', res.data[0].wr);
+        wx.setStorageSync('cw', res.data[0].cw);
 
-              if (getCurrentPages().length != 0) {
-                //刷新当前页面的数据
-                getCurrentPages()[getCurrentPages().length - 1].onLoad()
-              }
-              console.log(app.globalData.cw)
-              console.log(app.globalData.ow)
-              console.log(app.globalData.sw)
-              console.log(app.globalData.tg)
-              console.log(app.globalData.wn)
-              console.log(app.globalData.wr)
-            }
-          })
 
-        } else {
-          wx.setStorageSync('cw', res.data[0].cw);
-          wx.setStorageSync('ow', res.data[0].ow);
-          wx.setStorageSync('sw', res.data[0].sw);
-          wx.setStorageSync('tg', res.data[0].tg);
-          wx.setStorageSync('wn', res.data[0].wn);
-          wx.setStorageSync('wr', res.data[0].wr);
-
-          if (getCurrentPages().length != 0) {
-            //刷新当前页面的数据
-            getCurrentPages()[getCurrentPages().length - 1].onLoad()
-          }
-          console.log(app.globalData.cw)
-          console.log(app.globalData.ow)
-          console.log(app.globalData.sw)
-          console.log(app.globalData.tg)
-          console.log(app.globalData.wn)
-          console.log(app.globalData.wr)
+        if (getCurrentPages().length != 0) {
+          //刷新当前页面的数据
+          getCurrentPages()[getCurrentPages().length - 1].onLoad()
         }
+
       }
     })
   },

06213ff79fc550632f7a9c3eb287c48b95c7543a
HUANG Xiaodong <sheldonhuang@qq.com>
2019-08-10 15:14:41 +0200

    背单词页面样式调整

--- pages/vocab/vocab_result.js
+++ pages/vocab/vocab_result.js
@@ -17,12 +17,14 @@ Page({
     learn_lj_fr: null,
     learn_word_all: null,
     learn_word_no: null,
+    learn_word_cx: null,
+    learn_lj: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var learn_word = app.globalData.learn_word;
     var consult_data = app.globalData.consult_data;
     var learn_cx = consult_data[0].w_cx;
@@ -33,7 +35,41 @@ Page({
     var learn_word_all = consult_data[0].word;
     var learn_word_no = consult_data[0].w_no;
 
+
+
+    learn_word_all = learn_word_all.split(";");
+    learn_cx = learn_cx.split(";");
+    var learn_word_cx = [];
+    for (var i = 0; i < learn_word_all.length; i++) {
+      learn_word_cx.push(i + 1)
+      learn_word_cx.push(". ")
+      learn_word_cx.push(learn_word_all[i])
+      learn_word_cx.push(learn_cx[i])
+      learn_word_cx.push("\r\n")
+    }
+    learn_word_cx = learn_word_cx.join(" ")
+    console.log(learn_word_cx)
+
+    learn_lj_fr = learn_lj_fr.split(";");
+    learn_lj_cn = learn_lj_cn.split(";");
+    var learn_lj = [];
+    if (learn_lj_fr == '') {
+      learn_lj.push("暂无例句")
+    } else {
+      for (var i = 0; i < learn_lj_fr.length; i++) {
+        learn_lj.push(i + 1)
+        learn_lj.push(". ")
+        learn_lj.push(learn_lj_fr[i])
+        learn_lj.push(learn_lj_cn[i])
+        learn_lj.push("\r\n")
+      }
+      learn_lj = learn_lj.join(" ")
+      console.log(learn_lj)
+    }
+
     this.setData({
+      learn_word_cx: learn_word_cx,
+      learn_lj: learn_lj,
       learn_word: learn_word,
       learn_cx: learn_cx,
       learn_js_cn: learn_js_cn,
@@ -49,51 +85,51 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
 
-  JNSP: function () {
+  JNSP: function() {
     wx.redirectTo({
       url: 'vocab_learn',
     })
   },
 
-  justSoSo: function () {
+  justSoSo: function() {
     wx.redirectTo({
       url: 'vocab_learn',
     })
   },
 
-  bien_enregistre: function () {
+  bien_enregistre: function() {
     wx.redirectTo({
       url: 'vocab_learn',
     })
   },
 
-  retour: function () {
+  retour: function() {
     wx.redirectTo({
       url: '../index/index',
     })
@@ -103,7 +139,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })

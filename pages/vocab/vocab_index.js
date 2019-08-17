7a81b42fa5fa3619e72c930aff2553d7d5fb7fa6
HUANG Xiaodong <sheldonhuang@qq.com>
2019-08-18 00:45:34 +0200

    vocab_index页面能够显示信息

--- pages/vocab/vocab_index.js
+++ pages/vocab/vocab_index.js
@@ -5,7 +5,11 @@ const word_frequence = require('../../data/word_frequence.js')
 
 Page({
   data: {
-    book_id: null,
+    main_today_all: null,
+    main_already_word: null,
+    main_new_word: null,
+    main_review_word: null,
+    learn_word_new_today: [],
   },
 
   onLoad: function(options) {
@@ -16,10 +20,6 @@ Page({
 
     if (app.globalData.word_frequence_5000 == '') {
       this.new_user_data()
-    }
-
-    if (learn_word_today.length == 1) {
-      this.success();
     }
 
     var repeat_date = new Date();
@@ -73,14 +73,24 @@ Page({
 
     //加上旧词
     console.log(app.globalData.learn_word_new_today)
-    var learn_word_today = app.globalData.learn_word_new_today
-    var learn_word_today_no = app.globalData.learn_word_new_today_no
+    var learn_word_today = [repeat_date]
+    var learn_word_today_no = []
     var word_frequence_5000 = app.globalData.word_frequence_5000
+    var already_word = []
+    var today_all = []
+    var review_word = []
     for (var i = 0; i < 5000; i++) {
       if (word_frequence_5000[i].date == repeat_date && word_frequence_5000[i].level != 0) {
+        review_word.push(word_frequence_5000[i].learn_word)
+        console.log(word_frequence_5000[i].learn_word)
+      }
+      if (word_frequence_5000[i].level != 0) {
+        already_word.push(word_frequence_5000[i].learn_word)
+      }
+      if (word_frequence_5000[i].date == repeat_date) {
         learn_word_today.push(word_frequence_5000[i].learn_word)
         learn_word_today_no.push(i)
-        console.log(word_frequence_5000[i].learn_word)
+        today_all.push(word_frequence_5000[i].learn_word)
       }
     }
     console.log(learn_word_today)
@@ -88,6 +98,23 @@ Page({
 
     wx.setStorageSync('learn_word_today', learn_word_today)
     wx.setStorageSync('learn_word_today_no', learn_word_today_no)
+
+
+    var main_today_all = today_all.length
+    var main_already_word = already_word.length
+    var main_review_word = review_word.length
+    var main_new_word = today_all.length - review_word.length
+    var learn_word_new_today = wx.getStorageSync('learn_word_new_today')
+    learn_word_new_today.splice(0,1)
+
+    this.setData({
+      main_today_all: main_today_all,
+      main_already_word: main_already_word,
+      main_new_word: main_new_word,
+      main_review_word: main_review_word,
+      learn_word_new_today: learn_word_new_today,
+    })
+
 
   },
 
@@ -119,7 +146,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    if (getCurrentPages().length != 0) {
+      //刷新当前页面的数据
+      getCurrentPages()[getCurrentPages().length - 1].onLoad()
+    }
   },
 
   /**
@@ -134,12 +164,6 @@ Page({
    */
   onUnload: function() {
 
-  },
-
-  success: function() {
-    wx.redirectTo({
-      url: '../vocab/vocab_success',
-    })
   },
 
   start: function() {

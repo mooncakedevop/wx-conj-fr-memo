4d56ad4b2cf5d94e695e3751b72e91ec3b10f27d
huang@xiaodong.sh.cn <sheldonhuang@qq.com>
2020-06-03 14:51:31 +0200

    修改部分细节

--- pages/settings/help/help_detail.js
+++ pages/settings/help/help_detail.js
@@ -1,71 +1,61 @@
 // pages/settings/help/help.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+Page({
 
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
     dark_mode: null,
   },
+  
+  onLoad() {
+    var settings_new = wx.getStorageSync('settings_new')
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+    this.setData({
+      dark_mode: settings_new[0].dark_mode,
+    })
+  },
 
-    onShareAppMessage: function (res) {
-      return {
-        title: '有它!再也不用自己作决定了！😱',
-        path: 'pages/welcome/welcome',
-        imageUrl: '',
-        success: function (shareTickets) {
-          console.info(shareTickets + '成功');
-          // 转发成功
-        },
-        fail: function (res) {
-          console.log(res + '失败');
-          // 转发失败
-        },
-        complete: function (res) {
-          // 不管成功失败都会执行
-        }
+  onShareAppMessage: function (res) {
+    return {
+      title: '有它!再也不用自己作决定了！😱',
+      path: 'pages/welcome/welcome',
+      imageUrl: '',
+      success: function (shareTickets) {
+        console.info(shareTickets + '成功');
+        // 转发成功
+      },
+      fail: function (res) {
+        console.log(res + '失败');
+        // 转发失败
+      },
+      complete: function (res) {
+        // 不管成功失败都会执行
       }
-    },
+    }
+  },
 
-    backLastPage: function(){
-      wx.navigateBack({
-        delta: 1
-      })
-    },
+  backLastPage: function () {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
 
 
-    copy_current: function () {
-      var self = this;
-      wx.setClipboardData({
-        data: "hxdred",
-        success: function (res) {
-          wx.showModal({
-            title: '提示',
-            content: '✨复制成功✨请粘贴到微信搜一搜',
-            success: function (res) {
-              if (res.confirm) {
-                console.log('确定')
-              } else if (res.cancel) {
-                console.log('取消')
-              }
+  copy_current: function () {
+    var self = this;
+    wx.setClipboardData({
+      data: "hxdred",
+      success: function (res) {
+        wx.showModal({
+          title: '提示',
+          content: '✨复制成功✨请粘贴到微信搜一搜',
+          success: function (res) {
+            if (res.confirm) {
+              console.log('确定')
+            } else if (res.cancel) {
+              console.log('取消')
             }
-          })
-        }
-      })
-    },
-
-  }
-
-
+          }
+        })
+      }
+    })
+  },
 })

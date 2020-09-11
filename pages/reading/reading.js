11843fd9c2da1e49da5fc7949fd08828fc34510e
huang@xiaodong.sh.cn <sheldonhuang@qq.com>
2020-09-11 22:35:15 +0200

    🚧 修改文章显示页

--- pages/reading/reading.js
+++ pages/reading/reading.js
@@ -1,31 +1,22 @@
 const app = getApp()
 const db = wx.cloud.database() //初始化数据库
 const settings = db.collection('user_setting')
-const picker = new Array('5', '10', '20', '30', '45', '60')
 // 在页面中定义插屏广告
 let interstitialAd = null
 
 Page({
-
   data: {
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: '',
-
-    index: null,
-    picker: ['5', '10', '20', '30', '45', '60'],
-    hidden_or_not: null,
-
-    msg: null,
+    title:null,
+    author:null,
+    summary:null,
+    date:null,
+    contents: null,
     dark_mode: null,
   },
 
   onLoad() {
-    var settings_new = wx.getStorageSync('settings_new')
-    app.globalData.openid = wx.getStorageSync('openid')
-
-    this.onQuery_msg();
+    var settings_new = wx.getStorageSync('settings_new');
+    this.onQuery_article();
 
     this.setData({
       dark_mode: settings_new[0].dark_mode,
@@ -42,38 +33,20 @@ Page({
     }
   },
 
-  dark_mode: function(e) {
-    var settings_new = wx.getStorageSync('settings_new')
-    if (e.detail.value == true) {
-      settings_new[0].dark_mode = true;
-      wx.setStorageSync('settings_new', settings_new)
-    } else {
-      settings_new[0].dark_mode = false;
-      wx.setStorageSync('settings_new', settings_new)
-    }
-
-    this.setData({
-      dark_mode: settings_new[0].dark_mode,
-    })
-    this.successToast();
-
-    if (getCurrentPages().length != 0) {
-      //刷新当前页面的数据
-      getCurrentPages()[getCurrentPages().length - 1].onLoad()
-    }
-  },
-
-  onQuery_msg: function(search_word) {
+  onQuery_article: function() {
     var that = this
     const db = wx.cloud.database()
     const _ = db.command
-    db.collection('setting_msg').get({
+    db.collection('reading_articles').get({
       success: function(res) {
         console.log(res.data)
-        app.globalData.msg_data = res.data;
         that.setData({
-          msg: res.data[0].msg,
-        })
+          title: res.data[0].title,
+          author: res.data[0].author,
+          date: res.data[0].date,
+          summary: res.data[0].summary,
+          contents: res.data[0].contents,
+         })
       }
     })
   },
